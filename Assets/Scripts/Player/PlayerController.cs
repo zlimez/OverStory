cafb5ff6f800666a3de4aaeacb25b304bfbf37cf
Mmaimer33 <mmaimer33@gmail.com>
2024-09-10 15:52:31 +0800

    Improve jumping: add flexibility

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -5,270 +5,315 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour, PlayerControls.IPlayerActions
 {
-    #region Fields
-    private enum State
-    {
-        Idle,
-        Walk,
-        Run,
-        Jump,
-        Dash
-    }
+	#region Fields
+	
+	private enum State
+	{
+		Idle,
+		Walk,
+		Run,
+		Jump,
+		Dash
+	}
+	
+	// GameObject Components
+	private Rigidbody2D rb2D;
 
-    [Header("Animation")]
-    [SerializeField]
-    private apPortrait portrait;
+	// Animation support
+	[Header("Animation")]
+	[SerializeField]
+	private apPortrait portrait;
 
-    [SerializeField]
-    private float crossFadeSeconds = .01f;
+	[SerializeField]
+	private float crossFadeSeconds = .01f;
 
-    private Rigidbody2D rb2D;
+	// Movement support
+	[Header("Movement")]
+	[SerializeField]
+	private float walkSpeed = 4f;
 
-    // Movement support
-    [Header("Movement")]
-    [SerializeField]
-    private float walkSpeed = 4f;
+	[SerializeField]
+	private float runSpeed = 8f;
+	private Vector2 currVelocity;
+	private bool shouldRun;
 
-    [SerializeField]
-    private float runSpeed = 8f;
-    private Vector2 currVelocity;
-    private bool shouldRun;
+	private float currHorizontalSpeed;
+	private bool isFacingLeft;
 
-    private float currHorizontalSpeed;
-    private bool isFacingLeft;
+	// Jump support
+	[SerializeField]
+	private float jumpForce = 25f;
+	[SerializeField]
+	private float initialJumpForce = 1800f;
+	private const float MAX_JUMP_TIME = 0.3f;
+	private float remainingJumpTime = MAX_JUMP_TIME;
+	private bool isGrounded;
+	private bool isJumping;
 
-    // Jump support
-    [SerializeField]
-    private float jumpForce = 200f;
-    private bool isGrounded;
+	// Dash support
+	[SerializeField]
+	private float dashForce = 3000f;
+	private bool isDashAvailable = true;
+	private const float MAX_DASH_COOLDOWN = 1.2f;
+	private float dashCooldown = MAX_DASH_COOLDOWN;
 
-    // Dash support
-    [SerializeField]
-    private float dashForce = 3000f;
-    private bool isDashAvailable = true;
-    private float dashCooldown = 1.2f;
+	private State currState;
 
-    private State currentState;
-
-    #endregion
-
-
-    #region Methods
-
-    private void Awake()
-    {
-        rb2D = GetComponent<Rigidbody2D>();
-        currVelocity = new Vector2();
-
-        portrait.Initialize();
-        currentState = State.Idle;
-    }
-
-    private void Update()
-    {
-        HandleState();
-        if (rb2D.velocity.x > 0 && isFacingLeft)
-        {
-            FlipSprite();
-        }
-        else if (rb2D.velocity.x < 0 && !isFacingLeft)
-        {
-            FlipSprite();
-        }
-
-        if (!isDashAvailable)
-        {
-            dashCooldown -= Time.deltaTime;
-        }
-        if (dashCooldown <= 0)
-        {
-            isDashAvailable = true;
-            dashCooldown = 2f;
-        }
-    }
-
-    private void HandleState()
-    {
-        switch (currentState)
-        {
-            case State.Idle:
-                if (currHorizontalSpeed != 0)
-                {
-                    TransitionToState(State.Walk);
-                }
-                break;
-
-            case State.Walk:
-                if (Mathf.Abs(currHorizontalSpeed) > walkSpeed + .1f)
-                {
-                    TransitionToState(State.Run);
-                }
-                else if (currHorizontalSpeed == 0)
-                {
-                    TransitionToState(State.Idle);
-                }
-                break;
-
-            case State.Run:
-                if (Mathf.Abs(currHorizontalSpeed) <= walkSpeed - 0.1f)
-                {
-                    TransitionToState(State.Walk);
-                }
-                break;
-
-            case State.Jump:
-                if (isGrounded && Mathf.Abs(currVelocity.y) < .1f)
-                {
-                    TransitionToState(State.Idle);
-                }
-                break;
-
-            case State.Dash:
-                if (isDashAvailable)
-                {
-                    TransitionToState(State.Idle);
-                }
-                break;
-        }
-
-        // Update movement
-        currVelocity.y = rb2D.velocity.y;
-        currVelocity.x = currHorizontalSpeed;
-        rb2D.velocity = currVelocity;
-    }
-
-    private void TransitionToState(State newState)
-    {
-        Debug.Log($"Changing to state {newState.ToString()}");
-        currentState = newState;
-        PlayAnimation(newState.ToString());
-    }
-
-    private void OnCollisionEnter2D(Collision2D coll2D)
-    {
-        if (coll2D.gameObject.tag == "Ground")
-        {
-            isGrounded = true;
-        }
-    }
-
-    private void OnCollisionExit2D(Collision2D coll2D)
-    {
-        if (coll2D.gameObject.tag == "Ground")
-        {
-            isGrounded = false;
-        }
-    }
-
-    private void FlipSprite()
-    {
-        Vector3 currScale = gameObject.transform.localScale;
-        currScale.x *= -1;
-        gameObject.transform.localScale = currScale;
-
-        isFacingLeft = !isFacingLeft;
-    }
-
-    #endregion
+	#endregion
 
 
-    #region Event Handlers
+	#region Methods
 
-    public void OnJump(InputAction.CallbackContext context)
-    {
-        if (
-            isGrounded
-            && (
-                currentState == State.Idle
-                || currentState == State.Walk
-                || currentState == State.Run
-            )
-        )
-        {
-            rb2D.AddForce(Vector2.up * jumpForce);
-            isGrounded = false;
-            TransitionToState(State.Jump);
-        }
-    }
+	private void Awake()
+	{
+		rb2D = GetComponent<Rigidbody2D>();
+		currVelocity = new Vector2();
 
-    public void OnRun(InputAction.CallbackContext context)
-    {
-        shouldRun = context.ReadValueAsButton();
+		portrait.Initialize();
+		currState = State.Idle;
+	}
 
-        if (shouldRun && currentState == State.Walk)
-        {
-            currHorizontalSpeed = shouldRun ? runSpeed : walkSpeed;
-            currHorizontalSpeed *= isFacingLeft ? -1 : 1;
-            TransitionToState(State.Run);
-        }
-        else if (!shouldRun && currentState == State.Run)
-        {
-            TransitionToState(State.Walk);
-        }
-    }
+	private void Update()
+	{
+		HandleState();
+		if (rb2D.velocity.x > 0 && isFacingLeft)
+		{
+			FlipSprite();
+		}
+		else if (rb2D.velocity.x < 0 && !isFacingLeft)
+		{
+			FlipSprite();
+		}
 
-    public void OnMove(InputAction.CallbackContext context)
-    {
-        float maxSpeed = shouldRun ? runSpeed : walkSpeed;
-        currHorizontalSpeed = context.ReadValue<float>() * maxSpeed;
+		if (!isDashAvailable)
+		{
+			dashCooldown -= Time.deltaTime;
+		}
+		if (dashCooldown <= 0)
+		{
+			isDashAvailable = true;
+			dashCooldown = MAX_DASH_COOLDOWN;
+		}
+		
+		if (isJumping && remainingJumpTime > 0f) 
+		{
+			// Continue jump to max
+			rb2D.AddForce(Vector2.up * jumpForce);
+			remainingJumpTime -= Time.deltaTime;
+		}
+	}
 
-        if (currentState == State.Idle || currentState == State.Walk || currentState == State.Run)
-        {
-            TransitionToState(Mathf.Abs(currHorizontalSpeed) > 0 ? State.Walk : State.Idle);
-        }
-    }
+	private void HandleState()
+	{
+		switch (currState)
+		{
+			case State.Idle:
+				if (currHorizontalSpeed != 0)
+				{
+					TransitionToState(State.Walk);
+				}
+				break;
 
-    private void PlayAnimation(string animToPlay)
-    {
-        try
-        {
-            apAnimPlayData animData = portrait.CrossFadeAt(
-                animToPlay,
-                frame: portrait.GetAnimationCurrentFrame(animToPlay), // This so that the walk animation and run animation seamlessly transition at the same offset. Frame 3 is always when the back arm fully swings back
-                crossFadeSeconds
-            );
-            if (animData == null)
-            {
-                Debug.LogWarning("Failed to play animation " + animToPlay);
-            }
-        }
-        catch (Exception)
-        {
-            Debug.LogWarning(
-                $"Error playing animation {animToPlay}. The portrait is likely not initialized"
-            );
-        }
-    }
+			case State.Walk:
+				if (Mathf.Abs(currHorizontalSpeed) > walkSpeed + .1f)
+				{
+					TransitionToState(State.Run);
+				}
+				else if (currHorizontalSpeed == 0)
+				{
+					TransitionToState(State.Idle);
+				}
+				break;
 
-    public void OnDash(InputAction.CallbackContext context)
-    {
-        if (
-            isDashAvailable
-            && (
-                currentState == State.Idle
-                || currentState == State.Walk
-                || currentState == State.Run
-            )
-        )
-        {
-            Vector2 forceToAdd = isFacingLeft
-                ? Vector2.left * dashForce
-                : Vector2.right * dashForce;
-            rb2D.AddForce(forceToAdd);
-            isDashAvailable = false;
-            TransitionToState(State.Dash);
-        }
-    }
+			case State.Run:
+				if (Mathf.Abs(currHorizontalSpeed) <= walkSpeed - 0.1f)
+				{
+					TransitionToState(State.Walk);
+				}
+				break;
 
-    public void OnLook(InputAction.CallbackContext context)
-    {
-        throw new NotImplementedException();
-    }
+			case State.Jump:
+				if (isGrounded && Mathf.Abs(currVelocity.y) < .1f)
+				{
+					TransitionToState(State.Idle);
+				}
+				break;
 
-    public void OnFire(InputAction.CallbackContext context)
-    {
-        throw new NotImplementedException();
-    }
+			case State.Dash:
+				if (isDashAvailable)
+				{
+					TransitionToState(State.Idle);
+				}
+				break;
+		}
 
-    #endregion
+		// Update movement
+		currVelocity.y = rb2D.velocity.y;
+		currVelocity.x = currHorizontalSpeed;
+		rb2D.velocity = currVelocity;
+	}
+
+	private void TransitionToState(State newState)
+	{
+		// Debug.Log($"Changing to state {newState.ToString()}");
+		currState = newState;
+		PlayAnimation(newState.ToString());
+	}
+
+	private void OnCollisionEnter2D(Collision2D coll2D)
+	{
+		// Check if player is on the ground
+		if (coll2D.gameObject.tag == "Ground")
+		{
+			isGrounded = true;
+		}
+	}
+
+	private void OnCollisionExit2D(Collision2D coll2D)
+	{
+		// Check if player is leaving the ground
+		if (coll2D.gameObject.tag == "Ground")
+		{
+			isGrounded = false;
+		}
+	}
+
+	private void FlipSprite()
+	{
+		Vector3 currScale = gameObject.transform.localScale;
+		currScale.x *= -1;
+		gameObject.transform.localScale = currScale;
+
+		isFacingLeft = !isFacingLeft;
+	}
+	
+	private void PlayAnimation(string animToPlay)
+	{
+		try
+		{
+			apAnimPlayData animData = portrait.CrossFadeAt(
+				animToPlay,
+				frame: portrait.GetAnimationCurrentFrame(animToPlay), // This so that the walk animation and run animation seamlessly transition at the same offset. Frame 3 is always when the back arm fully swings back
+				crossFadeSeconds
+			);
+			if (animData == null)
+			{
+				Debug.LogWarning("Failed to play animation " + animToPlay);
+			}
+		}
+		catch (Exception)
+		{
+			Debug.LogWarning(
+				$"Error playing animation {animToPlay}. The portrait is likely not initialized"
+			);
+		}
+	}
+
+	#endregion
+
+
+	#region Event Handlers
+
+	public void OnJump(InputAction.CallbackContext context)
+	{
+		if (context.started)
+		{
+			Debug.Log("Jump context - started");
+			// Start jumping
+			if (isGrounded
+			&& (
+				currState == State.Idle
+				|| currState == State.Walk
+				|| currState == State.Run
+			))
+			{
+				rb2D.AddForce(Vector2.up * initialJumpForce);
+				remainingJumpTime = MAX_JUMP_TIME;
+				isJumping = true;
+				TransitionToState(State.Jump);
+			}
+		}
+		else if (context.canceled) 
+		{
+			Debug.Log("Jump context - canceled");
+			// Stop jump and reset jumpTime
+			isJumping = false;
+			remainingJumpTime = 0f;
+		}
+		
+		/*
+		// TODO: remove old code below
+		if (isGrounded
+			&& (
+				currState == State.Idle
+				|| currState == State.Walk
+				|| currState == State.Run
+			)
+		)
+		{
+			rb2D.AddForce(Vector2.up * jumpForce);
+			isGrounded = false;
+			TransitionToState(State.Jump);
+		}
+		*/
+	}
+
+	public void OnRun(InputAction.CallbackContext context)
+	{
+		shouldRun = context.ReadValueAsButton();
+
+		if (shouldRun && currState == State.Walk)
+		{
+			currHorizontalSpeed = shouldRun ? runSpeed : walkSpeed;
+			currHorizontalSpeed *= isFacingLeft ? -1 : 1;
+			TransitionToState(State.Run);
+		}
+		else if (!shouldRun && currState == State.Run)
+		{
+			TransitionToState(State.Walk);
+		}
+	}
+
+	public void OnMove(InputAction.CallbackContext context)
+	{
+		float maxSpeed = shouldRun ? runSpeed : walkSpeed;
+		currHorizontalSpeed = context.ReadValue<float>() * maxSpeed;
+
+		if (currState == State.Idle || currState == State.Walk || currState == State.Run)
+		{
+			TransitionToState(Mathf.Abs(currHorizontalSpeed) > 0 ? State.Walk : State.Idle);
+		}
+	}
+
+	public void OnDash(InputAction.CallbackContext context)
+	{
+		if (isDashAvailable
+			&& (
+				currState == State.Idle
+				|| currState == State.Walk
+				|| currState == State.Run
+				|| currState == State.Jump
+			)
+		)
+		{
+			Vector2 forceToAdd = isFacingLeft
+				? Vector2.left * dashForce
+				: Vector2.right * dashForce;
+			rb2D.AddForce(forceToAdd);
+			isDashAvailable = false;
+			TransitionToState(State.Dash);
+		}
+	}
+
+	public void OnLook(InputAction.CallbackContext context)
+	{
+		throw new NotImplementedException();
+	}
+
+	public void OnFire(InputAction.CallbackContext context)
+	{
+		throw new NotImplementedException();
+	}
+
+	#endregion
 }
