f5113dad0e3724be6ce7049b1f3246eb0f47c4ab
Mmaimer33 <mmaimer33@gmail.com>
2024-10-23 19:48:30 +1000

    Add cluster interaction to Weapon script

--- Assets/Scripts/Player/Weapon.cs
+++ Assets/Scripts/Player/Weapon.cs
@@ -5,56 +5,63 @@ using UnityEngine;
 
 namespace Abyss.Player
 {
-    public class Weapon : MonoBehaviour
-    {
-        // TODO: Add removable obstacle to mask, currently only inc. enemy
-        static readonly int _layerMask = 1 << 6;
-        public WeaponItem weaponItem;
-        readonly HashSet<int> _hits = new();
-        ParticleSystem _particleSystem;
+	public class Weapon : MonoBehaviour
+	{
+		// TODO: Add removable obstacle to mask, currently only inc. enemy
+		static readonly int _layerMask = 1 << 6;
+		public WeaponItem weaponItem;
+		readonly HashSet<int> _hits = new();
+		ParticleSystem _particleSystem;
+		[SerializeField] private int breakableDamage = 1;
 
-        void Awake() => _particleSystem = GetComponent<ParticleSystem>();
+		void Awake() => _particleSystem = GetComponent<ParticleSystem>();
 
-        void OnEnable() => EventManager.StartListening(new GameEvent(WeaponItem.WeaponEquippedPrefix), Equip);
-        void OnDisable() => EventManager.StopListening(new GameEvent(WeaponItem.WeaponEquippedPrefix), Equip);
+		void OnEnable() => EventManager.StartListening(new GameEvent(WeaponItem.WeaponEquippedPrefix), Equip);
+		void OnDisable() => EventManager.StopListening(new GameEvent(WeaponItem.WeaponEquippedPrefix), Equip);
 
-        void Equip(object obj)
-        {
-            if (weaponItem != null) GameManager.Instance.Inventory.AddTo(weaponItem);
-            weaponItem = (WeaponItem)obj;
-        }
+		void Equip(object obj)
+		{
+			if (weaponItem != null) GameManager.Instance.Inventory.AddTo(weaponItem);
+			weaponItem = (WeaponItem)obj;
+		}
 
-        public void Strike(float str)
-        {
-            if (weaponItem == null) return;
-            // TODO: Change position based on weapon movement
-            var hitEnemies = Physics2D.OverlapCircleAll(transform.position, weaponItem.radius, _layerMask);
-            bool psPlayed = false;
-            foreach (var hitEnemy in hitEnemies)
-            {
-                if (hitEnemy.gameObject.TryGetComponent<EnemyPart>(out var enemyPart))
-                {
-                    if (_hits.Contains(enemyPart.EnemyIntanceId)) continue;
-                    if (!psPlayed)
-                    {
-                        _particleSystem.Play();
-                        psPlayed = true;
-                    }
-                    _hits.Add(enemyPart.EnemyIntanceId);
-                    enemyPart.TakeHit(weaponItem.damage + str);
-                }
-            }
-        }
+		public void Strike(float str)
+		{
+			if (weaponItem == null) return;
+			// TODO: Change position based on weapon movement
+			var hitEnemies = Physics2D.OverlapCircleAll(transform.position, weaponItem.radius, _layerMask);
+			bool psPlayed = false;
+			foreach (var hitEnemy in hitEnemies)
+			{
+				Debug.Log(hitEnemy.name);
+				if (hitEnemy.gameObject.TryGetComponent<EnemyPart>(out var enemyPart))
+				{
+					if (_hits.Contains(enemyPart.EnemyIntanceId)) continue;
+					if (!psPlayed)
+					{
+						_particleSystem.Play();
+						psPlayed = true;
+					}
+					_hits.Add(enemyPart.EnemyIntanceId);
+					enemyPart.TakeHit(weaponItem.damage + str);
+				}
+				else if (hitEnemy.gameObject.CompareTag("Breakable")) 
+				{
+					hitEnemy.gameObject.TryGetComponent<Cluster>(out var clusterComponent);
+					clusterComponent.TakeHit(breakableDamage);
+				}
+			}
+		}
 
-        public void Reset() => _hits.Clear();
+		public void Reset() => _hits.Clear();
 
 #if UNITY_EDITOR
-        void OnDrawGizmos()
-        {
-            Gizmos.color = Color.red;
-            if (weaponItem != null)
-                Gizmos.DrawWireSphere(transform.position, weaponItem.radius);
-        }
+		void OnDrawGizmos()
+		{
+			Gizmos.color = Color.red;
+			if (weaponItem != null)
+				Gizmos.DrawWireSphere(transform.position, weaponItem.radius);
+		}
 #endif
-    }
+	}
 }
